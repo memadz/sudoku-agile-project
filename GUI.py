16566739391bb9475b93b08556a4a621d7a2737c
Berhan Ahmed <berhana@student.chalmers.se>
2024-11-21 18:10:57 +0000

    GUI integrated with the number generated.
    Courtesy of team 2

---
+++ GUI.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+import boardnumbergenerator
+
+root = tk.Tk()
+root.title("Sudoku")
+root.geometry("300x400")
+
+GRID_SIZE = 9
+
+sudoku_grid = boardnumbergenerator.solved_sudoko()
+
+frame = tk.Frame(root, padx=5, pady=5)
+frame.pack(expand=True)
+
+for row in range(GRID_SIZE):
+    for col in range(GRID_SIZE):
+        entry = tk.Entry(frame, width=2, justify="center", font=("Arial", 14))
+        entry.grid(row=row, column=col, padx=1, pady=1)
+        entry.insert(row, sudoku_grid[row][col])
+root.mainloop()
